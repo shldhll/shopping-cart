42fb223c1b781b1e11871705fabc8742587a2d0c
Sahil Dhull <iam.shldhll@gmail.com>
2023-07-19 02:55:55 +0530

    base layout with bg image and panels

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,15 @@
+import { IProduct, products } from "@/data/products";
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <p>Hello world</p>
+    <div className="h-screen w-screen bg-[url('/background.jpg')] flex items-center justify-center">
+      <div className="h-[75%] w-[75%] flex">
+        <div className="h-full w-[70%] bg-white flex flex-col items-center space-y-8">
+        </div>
+        <div className="h-full w-[30%] bg-[#d9d9d9]">
+        </div>
+      </div>
+    </div>
   )
 }
