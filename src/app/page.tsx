23945906ae681e85c3ff83c3eba21922520ca2b7
Sahil Dhull <iam.shldhll@gmail.com>
2023-07-19 03:27:30 +0530

    table layout on left panel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,35 @@ export default function Home() {
     <div className="h-screen w-screen bg-[url('/background.jpg')] flex items-center justify-center">
       <div className="h-[75%] w-[75%] flex">
         <div className="h-full w-[70%] bg-white flex flex-col items-center space-y-8">
+          <p className="mt-8 pb-4 text-xl font-medium border-b-[2px] w-[90%]">Shirt</p>
+          <table className="w-[90%] font-light">
+            <thead>
+              <tr>
+                <th className="uppercase font-light text-xs text-gray-400 text-left">Product Details</th>
+                <th className="uppercase font-light text-xs text-gray-400 text-center">Quantity</th>
+                <th className="uppercase font-light text-xs text-gray-400 text-center">Price</th>
+                <th className="uppercase font-light text-xs text-gray-400 text-center">Total</th>
+              </tr>
+            </thead>
+            <tbody>
+              {
+                products.map((product: IProduct) => (
+                  <tr key={product.code}>
+                    <td className="flex items-center pt-8 space-x-4">
+                      <img className="rounded-md border border-gray-300" src={product.thumb} />
+                      <div className="flex flex-col">
+                        <span className="text-[#734ffc]">{product.name}</span>
+                        <span className="text-gray-400 text-xs">Product code {product.code}</span>
+                      </div>
+                    </td>
+                    <td className="text-center pt-8">1</td>
+                    <td className="text-center pt-8">{product.price} €</td>
+                    <td className="text-center pt-8">0 €</td> {/* TODO: calculate total price */}
+                  </tr>
+                ))
+              }
+            </tbody>
+          </table>
         </div>
         <div className="h-full w-[30%] bg-[#d9d9d9]">
         </div>
