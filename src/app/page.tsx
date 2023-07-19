6e4f58b04a4013d04491ab31063532d0cafa4ec9
Sahil Dhull <iam.shldhll@gmail.com>
2023-07-19 16:29:57 +0530

    added quantity counter for products

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,44 @@
+'use client'
+
 import { IProduct, products } from "@/data/products";
-import Image from "next/image";
+import { useState } from "react";
 
 export default function Home() {
+  const [productQuantity, setProductQuantity] = useState<Record<string, number>>({});
+
+  const incrementProductQuantity = (productCode: string) => {
+    // Create a new key for the product if it doesn't exist
+    if (productQuantity[productCode] === undefined) {
+      setProductQuantity({
+        ...productQuantity,
+        [productCode]: 0
+      })
+    }
+
+    // It is safe to increment the quantity
+    setProductQuantity({
+      ...productQuantity,
+      [productCode]: productQuantity[productCode] + 1
+    })
+  }
+
+  const decrementProductQuantity = (productCode: string) => {
+    // Create a new key for the product if it doesn't exist
+    // and decrement only if the quantity is not 0
+    if (productQuantity[productCode] === undefined) {
+      setProductQuantity({
+        ...productQuantity,
+        [productCode]: 0
+      })
+    } else if (productQuantity[productCode] !== 0) {
+      setProductQuantity({
+        ...productQuantity,
+        [productCode]: productQuantity[productCode] - 1
+      })
+    }
+  }
+
+
   return (
     <div className="h-screen w-screen bg-[url('/background.jpg')] flex items-center justify-center">
       <div className="h-[75%] w-[75%] flex">
@@ -11,28 +48,36 @@ export default function Home() {
             <thead>
               <tr>
                 <th className="uppercase font-light text-xs text-gray-400 text-left">Product Details</th>
-                <th className="uppercase font-light text-xs text-gray-400 text-center">Quantity</th>
-                <th className="uppercase font-light text-xs text-gray-400 text-center">Price</th>
-                <th className="uppercase font-light text-xs text-gray-400 text-center">Total</th>
+                <th className="uppercase font-light text-xs text-gray-400 text-center pr-8">Quantity</th>
+                <th className="uppercase font-light text-xs text-gray-400 text-center pr-8">Price</th>
+                <th className="uppercase font-light text-xs text-gray-400 text-center pr-8">Total</th>
               </tr>
             </thead>
             <tbody>
-              {
-                products.map((product: IProduct) => (
-                  <tr key={product.code}>
-                    <td className="flex items-center pt-8 space-x-4">
-                      <img className="rounded-md border border-gray-300" src={product.thumb} />
-                      <div className="flex flex-col">
-                        <span className="text-[#734ffc]">{product.name}</span>
-                        <span className="text-gray-400 text-xs">Product code {product.code}</span>
-                      </div>
-                    </td>
-                    <td className="text-center pt-8">1</td>
-                    <td className="text-center pt-8">{product.price} €</td>
-                    <td className="text-center pt-8">0 €</td> {/* TODO: calculate total price */}
-                  </tr>
-                ))
-              }
+              {products.map((product: IProduct) => (
+                <tr key={product.code}>
+                  <td className="flex items-center pt-8 space-x-4">
+                    <img className="rounded-md border border-gray-300" src={product.thumb} />
+                    <div className="flex flex-col">
+                      <span className="text-[#734ffc]">{product.name}</span>
+                      <span className="text-gray-400 text-xs">Product code {product.code}</span>
+                    </div>
+                  </td>
+                  <td className="text-center pt-8 pr-8">
+                    <div className="flex items-center justify-center">
+                      <button className="text-[#734ffc] p-2 text-xl font-medium" onClick={() => { decrementProductQuantity(product.code) }}>-</button>
+                      <div className="flex border border-gray-200 w-10 h-10 text-center justify-center items-center rounded-md">{productQuantity[product.code] || 0}</div>
+                      <button className="text-[#734ffc] p-2 text-xl font-medium" onClick={() => { incrementProductQuantity(product.code) }}>+</button>
+                    </div>
+                  </td>
+                  <td className="text-center pt-8 pr-8">{product.price} €</td>
+                  <td className="text-center pt-8 pr-8">{
+                    productQuantity[product.code] === undefined
+                      ? 0
+                      : productQuantity[product.code] * product.price
+                  } €</td>
+                </tr>
+              ))}
             </tbody>
           </table>
         </div>
