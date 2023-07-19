73623707a7d11739cffe609b3979494e04a41273
Sahil Dhull <iam.shldhll@gmail.com>
2023-07-19 17:24:32 +0530

    created right panel ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,41 @@
 'use client'
 
 import { IProduct, products } from "@/data/products";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
   const [productQuantity, setProductQuantity] = useState<Record<string, number>>({});
+  const [totalCount, setTotalCount] = useState<number>(0);
+  const [totalPrice, setTotalPrice] = useState<number>(0);
+
+  useEffect(() => {
+    let count = 0;
+    let price = 0;
+
+    for (const productCode in productQuantity) {
+      const productPrice = products.find((product: IProduct) => product.code === productCode)?.price || 0;
+
+      count += productQuantity[productCode] === undefined ? 0 : productQuantity[productCode];
+      price += productQuantity[productCode] === undefined ? 0 : productQuantity[productCode] * productPrice;
+    }
+
+    setTotalCount(count);
+    setTotalPrice(price);
+  }, [productQuantity])
 
   const incrementProductQuantity = (productCode: string) => {
-    // Create a new key for the product if it doesn't exist
+    // Create a new key for the product and set the value to '1' if it doesn't exist
     if (productQuantity[productCode] === undefined) {
       setProductQuantity({
         ...productQuantity,
-        [productCode]: 0
+        [productCode]: 1
+      })
+    } else {
+      setProductQuantity({
+        ...productQuantity,
+        [productCode]: productQuantity[productCode] + 1
       })
     }
-
-    // It is safe to increment the quantity
-    setProductQuantity({
-      ...productQuantity,
-      [productCode]: productQuantity[productCode] + 1
-    })
   }
 
   const decrementProductQuantity = (productCode: string) => {
@@ -41,9 +57,10 @@ export default function Home() {
 
   return (
     <div className="h-screen w-screen bg-[url('/background.jpg')] flex items-center justify-center">
+      {/* Left panel */}
       <div className="h-[75%] w-[75%] flex">
         <div className="h-full w-[70%] bg-white flex flex-col items-center space-y-8">
-          <p className="mt-8 pb-4 text-xl font-medium border-b-[2px] w-[90%]">Shirt</p>
+          <p className="mt-8 pb-4 text-xl font-medium border-b-[2px] w-[90%]">Products</p>
           <table className="w-[90%] font-light">
             <thead>
               <tr>
@@ -81,7 +98,40 @@ export default function Home() {
             </tbody>
           </table>
         </div>
-        <div className="h-full w-[30%] bg-[#d9d9d9]">
+
+        {/* Right panel */}
+        <div className="h-full w-[30%] bg-[#d9d9d9] flex flex-col justify-between font-light">
+          <div className="w-full flex flex-col items-center">
+            <p className="mt-8 pb-4 text-xl font-medium border-b-[2px] border-[#bbbbc0] w-[80%]">Products</p>
+            <div className="flex w-[80%] py-8 justify-between border-b-[2px] border-[#bbbbc0]">
+              <span>{totalCount} Items</span>
+              <span>{totalPrice} €</span>
+            </div>
+            <div className="flex flex-col w-[80%] py-5 border-b-[2px] border-[#bbbbc0] space-y-4">
+              <span className="uppercase font-light text-sm text-gray-500">Discounts</span>
+              <div className="flex flex-col">
+                <div className="flex justify-between">
+                  <span>x3 T-Shirt offer</span>
+                  <span>- 0 €</span>
+                </div>
+              </div>
+              <div className="flex flex-col">
+                <div className="flex justify-between">
+                  <span>2x1 Cup offer</span>
+                  <span>- 0 €</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="w-full flex flex-col items-center">
+            <div className="flex w-[80%] mb-2 py-4 justify-between border-t-[2px] border-[#bbbbc0] items-center">
+              <span>Total</span>
+              <span className="text-xl">{totalPrice} €</span>
+            </div>
+            <button className="bg-[#734ffc] w-[80%] mb-10 p-3 rounded-md shadow text-gray-200 font-medium">
+              Checkout
+            </button>
+          </div>
         </div>
       </div>
     </div>
